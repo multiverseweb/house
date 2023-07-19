1eee6f12f18b83f92b2d6fe99c407a8bad9db3f4
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2023-07-19 21:20:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ alert('Hello there! This is my first project that is entirely based on JavaScrip
 window.onscroll = function() {
     scrollFunction()
   };
-  
+
   function scrollFunction() {
-    if (document.body.scrollTop > 200 ||
+    if (document.body.scrollTop > 10 ||
+      document.documentElement.scrollTop > 10) {
+        document.getElementById("a").style.width="80vw";
+      }
+      if (document.body.scrollTop < 10 ||
+        document.documentElement.scrollTop < 10){
+        document.getElementById("a").style.width="0vw";
+        document.getElementById("int").style.opacity="0";
+      }
+      if (document.body.scrollTop > 200 ||
         document.documentElement.scrollTop > 200) {
+          document.getElementById("int").style.opacity="1";
+      }
+
+    if (document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500) {
         document.getElementById("int").style.scale="0.6";
         document.getElementById("int").style.top="20vh";
         document.getElementById("int").style.marginLeft="23vw";
-        document.getElementById("a").style.width="140vw";
-        document.getElementById("a").style.left="-50vw";
-        document.getElementById("b").style.height="40vh";
+        document.getElementById("a").style.width="80vw";
+        document.getElementById("a").style.left="10vw";
+        document.getElementById("a").style.top="67.8vh";
+        document.getElementById("b").style.height="39vh";
         document.getElementById("c").style.height="60vh";
         document.getElementById("b").style.top="40vh";
         document.getElementById("c").style.top="20vh";
@@ -19,6 +34,10 @@ window.onscroll = function() {
         document.getElementById("x").style.top="43vh";
         document.getElementById("y").style.top="28vh";
         document.getElementById("z").style.top="28vh";
+        document.getElementById("a1").style.right="27vw";
+        document.getElementById("b1").style.right="27vw";
+        document.getElementById("c1").style.right="27vw";
+        document.getElementById("d1").style.right="27vw";
         document.getElementById("text").innerHTML="House With Garage";
       } 
     else {
@@ -27,7 +46,8 @@ window.onscroll = function() {
         document.getElementById("int").style.marginLeft="0vw";
         document.getElementById("a").style.width="80vw";
         document.getElementById("a").style.left="10vw";
-        document.getElementById("b").style.height="60vh";
+        document.getElementById("a").style.top="80vh";
+        document.getElementById("b").style.height="59vh";
         document.getElementById("c").style.height="45vh";
         document.getElementById("b").style.top="20vh";
         document.getElementById("c").style.top="35vh";
@@ -35,6 +55,10 @@ window.onscroll = function() {
         document.getElementById("x").style.top="62vh";
         document.getElementById("y").style.top="47vh";
         document.getElementById("z").style.top="47vh";
+        document.getElementById("a1").style.right="50vw";
+        document.getElementById("b1").style.right="50vw";
+        document.getElementById("c1").style.right="50vw";
+        document.getElementById("d1").style.right="50vw";
         document.getElementById("text").innerHTML="House";
       } 
-    }
+  }
